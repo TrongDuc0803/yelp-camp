21d8b426f52d0c04f9fb0d655e17605d58032083
Teomathe <trongducle080303@gmail.com>
2024-09-04 21:04:38 +0700

    Update app

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -3,7 +3,7 @@ maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
     container: 'cluster-map',
     style: maptilersdk.MapStyle.BRIGHT,
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [108.277199, 14.058324],
     zoom: 3
 });
 
